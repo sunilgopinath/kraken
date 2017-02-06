e5d6e5b25ec7713995ed264f8819efecd3e486db
Sunil Gopinath <sunilgopinath@R5006503.local>
2017-02-06 17:20:51 +1100

    change key

--- web/controllers/kraken_controller.ex
+++ web/controllers/kraken_controller.ex
@@ -2,7 +2,7 @@ defmodule Kraken.KrakenController do
   use Kraken.Web, :controller
 
   def index(conn, _params) do
-    case HTTPoison.get("https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=http%3A%2F%2Fwww.smh.com.au&screenshot=false&strategy=mobile&key=AIzaSyAadHE3pqlMUyhxX64aZwB8fVFqGy1Oyls") do
+    case HTTPoison.get("https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=http%3A%2F%2Fwww.smh.com.au&screenshot=false&strategy=mobile&key=") do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         Poison.Parser.parse!(body) |> IO.inspect
       {:ok, %HTTPoison.Response{status_code: 404}} ->
